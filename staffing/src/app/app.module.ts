4bd2ba8f4adad3e7e0ae4af83713f0da9338afcf
peachyvvalentine <55882490+peachyvvalentine@users.noreply.github.com>
2020-01-15 16:20:45 -0300

    a

--- staffing/src/app/app.module.ts
+++ staffing/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { VerproyectosComponent } from './verproyectos/verproyectos.component';
+import { LoginComponent } from './components/login/login.component';
+import { VerproyectosComponent } from './components/verproyectos/verproyectos.component';
 import { ComponentsComponent } from './components/components.component';
 import { RegistrocolComponent } from './components/registrocol/registrocol.component';
 import { CrearproyectosComponent } from './components/crearproyectos/crearproyectos.component';
@@ -14,12 +14,12 @@ import { ListarComponent } from './components/listar/listar.component';
 
 const appRoutes: Routes = [
   { path : 'login', component: LoginComponent},
-  { path : 'verproyectos', component: VerProyectosComponent},
-  { path : 'registrocol', component: RegistroColComponent},
-  { path : 'crearproyectos', component: CrearProyectosComponent},
+  { path : 'verproyectos', component: VerproyectosComponent},
+  { path : 'registrocol', component: RegistrocolComponent},
+  { path : 'crearproyectos', component: CrearproyectosComponent},
   { path : 'habilidaddes', component: HabilidadesComponent},
-  { path : 'reportestaff', component: ReporteStaffComponent},
-  { path : 'accesoadmin', component: AccesoAdminComponent},
+  { path : 'reportestaff', component: ReportestaffComponent},
+  { path : 'accesoadmin', component: AccesoadminComponent},
   { path : 'listar', component: ListarComponent},
   { path: '**', redirectTo: 'login' },
   ];
