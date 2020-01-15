f7b5f70d3c08be9f598c681ba30c883b3239b423
peachyvvalentine <55882490+peachyvvalentine@users.noreply.github.com>
2020-01-15 14:38:19 -0300

    a

--- staffing/src/app/app.module.ts
+++ staffing/src/app/app.module.ts
@@ -1,19 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { VerproyectosComponent } from './verproyectos/verproyectos.component';
 import { ComponentsComponent } from './components/components.component';
+import { RegistrocolComponent } from './components/registrocol/registrocol.component';
+import { CrearproyectosComponent } from './components/crearproyectos/crearproyectos.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ReportestaffComponent } from './components/reportestaff/reportestaff.component';
+import { AccesoadminComponent } from './components/accesoadmin/accesoadmin.component';
+import { ListarComponent } from './components/listar/listar.component';
+
+const appRoutes: Routes = [
+  { path : 'login', component: LoginComponent},
+  { path : 'verproyectos', component: VerProyectosComponent},
+  { path : 'registrocol', component: RegistroColComponent},
+  { path : 'crearproyectos', component: CrearProyectosComponent},
+  { path : 'habilidaddes', component: HabilidadesComponent},
+  { path : 'reportestaff', component: ReporteStaffComponent},
+  { path : 'accesoadmin', component: AccesoAdminComponent},
+  { path : 'listar', component: ListarComponent},
+  { path: '**', redirectTo: 'login' },
+  ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     VerproyectosComponent,
-    ComponentsComponent
+    ComponentsComponent,
+    RegistrocolComponent,
+    CrearproyectosComponent,
+    HabilidadesComponent,
+    ReportestaffComponent,
+    AccesoadminComponent,
+    ListarComponent
   ],
   imports: [
+    RouterModule.forRoot(  appRoutes, {enableTracing: false}),
     BrowserModule
   ],
   providers: [],
