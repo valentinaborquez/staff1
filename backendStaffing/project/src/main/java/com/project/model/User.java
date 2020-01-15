2b530139d82b4dff51b0240332b5444f5ba6a081
priscilla <priscillaconstanza.palomo@gmail.com>
2020-01-15 14:26:04 -0300

    backend

---
+++ backendStaffing/project/src/main/java/com/project/model/User.java
@@ -0,0 +1,42 @@
+package com.project.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name= "User")
+public class User {
+    @Id
+    @Column(name = "id_user")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_user;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+    @Column(name = "password" , nullable = false)
+    private String password;
+
+    public long getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(long id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
