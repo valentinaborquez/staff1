c9bb9fd91dcffd636c532e16181258fe6920f7e2
Millena13 <millena.yissell@gmail.com>
2020-01-22 17:03:42 -0300

    pro

--- backendStaffing/project/src/main/java/com/project/imp/ProjectImp.java
+++ backendStaffing/project/src/main/java/com/project/imp/ProjectImp.java
@@ -1,13 +1,25 @@
 package com.project.imp;
 
+import com.project.dto.ReqProjectDto;
+import com.project.dto.ResProjectDto;
+import com.project.model.Project;
 import com.project.service.IProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProjectImp implements IProjectService {
+
+    @Override
+    public List<Project> listarProject() throws Exception {
+        return null;
+    }
+
+    @Override
+    public ResProjectDto guardarProject(ReqProjectDto reqProjectDtoDto) throws Exception {
+        return null;
+    }
 }
-@Autowired
-private ProjectRepository projectRepository ;
-@Autowired
-private
+
--- backendStaffing/project/src/main/java/com/project/service/IProjectService.java
+++ backendStaffing/project/src/main/java/com/project/service/IProjectService.java
@@ -1,6 +1,12 @@
 package com.project.service;
 
+import com.project.dto.ReqProjectDto;
+import com.project.dto.ResProjectDto;
+import com.project.model.Project;
+import  java.util.List;
 public interface IProjectService {
 
+    List<Project> listarProject()throws Exception;
+    ResProjectDto guardarProject(ReqProjectDto reqProjectDtoDto) throws Exception;
 
 }
